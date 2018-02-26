92c6a2c3765380cf1cf6dc10d9a95e23317d39a0
Brian Ligh <bligh@globus.ib.int.bioinfo.ucr.edu>
2018-02-26 10:51:21 -0800

    added default setting for min length for trim galore and allows for no base end trim

--- trim-galore_dir_single.sh
+++ trim-galore_dir_single.sh
@@ -22,8 +22,8 @@ done
 ###############################################################
 # Step 2: Input argument checks and loading of necessary modules
 
-if [[ "$dir" == "" || "$len" == "" || "$ends" == "" ]]; then
-	echo "ERROR: Options -d -l -e  require arguments." >&2
+if [[ "$dir" == "" ]]; then
+	echo "ERROR: Option -d requires arguments." >&2
 	exit 1
 fi 
 
@@ -32,10 +32,16 @@ if [[ "$quality" == "" ]]; then
 	echo "No input for -q; quality set to 20"
 fi
 
+if [[ "$len" == "" ]]; then
+        length=35
+        echo "No input for -l; length set to 35"
+fi
+
 out=$dir
 
 
 module load fastqc/0.11.3 
+module load fastqc
 module load trim_galore/0.4.1
 
 
@@ -47,6 +53,16 @@ directory=$(readlink -f "$dir")
 
 ##############################################################
 #Step 4: Search directory for .gz files and submit them to cluster
+
+if [["$ends" == "" ]]; then
+	for f in $directory/*.gz
+	do
+  		echo $f
+        	sbatch -p highmem --mem=100g --time=15:00:00 --wrap "trim_galore --fastqc --gzip --length $len -o $out -q $quality $f"
+	done
+	exit 1
+fi
+
 
 for f in $directory/*.gz
 do
